65e12c84797b6ba48f21e048afb4f760bed298e6
Karl Bunnell <karlbunnell@c02n31fvfd59.internal.cacheflow.com>
2015-02-12 15:18:35 -0700

    Added master_only recipe so only master is started instead of master and all slaves

---
+++ recipes/master_only.rb
@@ -0,0 +1,29 @@
+
+include_recipe "spark::default"
+
+secret = Chef::EncryptedDataBagItem.load_secret(node["chef"]["data_bag_secret_path"])
+key = Chef::EncryptedDataBagItem.load("spark", "ssh_key", secret)
+spark_path = node['spark']['install_dir']
+spark_user = node["spark"]["user"]
+
+file "#{spark_path}/.ssh/id_#{key['type']}" do
+  mode "0400"
+  owner spark_user
+  content key['private_key']
+end
+
+file "#{spark_path}/conf/slaves" do
+  mode "0755"
+  owner spark_user
+  content(node['spark']['slaves'].join("\n") + "\n")
+end
+
+service "spark" do
+  start_command <<-BASH
+    sudo -u '#{spark_user}' -- sh -c 'cd #{spark_path} && ./sbin/start-master.sh'
+  BASH
+  stop_command <<-BASH
+    sudo -u '#{spark_user}' -- sh -c 'cd #{spark_path} && ./sbin/stop-master.sh'
+  BASH
+  action :start
+end
